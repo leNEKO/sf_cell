e26a6fbb49970405b3c37cca5bb342569bcf61cf
leNEKO <le.neko@gmail.com>
2018-09-17 14:24:04 +0200

    home page

--- src/Controller/CellController.php
+++ src/Controller/CellController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CellController extends AbstractController
 {
-
+    /**
+     * @Route("/", name= "app_home")
+     */
     public function home()
     {
         return $this->render("home.html.twig", [
